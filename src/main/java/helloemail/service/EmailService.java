5e7f6e2374b7b521e542b3c126b6bf621c66a4de
MiUsuario <abelardolg.it@gmail.com>
2025-10-08 18:42:47 +0200

    Instrucción innecesaria
    No hace falta especificar desde qué dirección se envía el mensaje, ya que la propiedad spring.mail.username (que la encuentras en application.properties) ya determina la cuenta de origen.

--- src/main/java/helloemail/service/EmailService.java
+++ src/main/java/helloemail/service/EmailService.java
@@ -13,7 +13,6 @@ public class EmailService {
     public void sendEmail(String to, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
 
-        message.setFrom("lunabonita547@gmail.com");
         message.setTo(to);
         message.setSubject(subject);
         message.setText(body);
